43412f1c04d95b94407403bcbaf2c941fc3967dd
GenesysJ <102426361+GenesysJ@users.noreply.github.com>
2023-11-10 08:19:53 +0100

    Update queries.py
    I just add some new queries for the graph generation

--- cpgqls_client/queries.py
+++ cpgqls_client/queries.py
@@ -34,3 +34,31 @@ def workspace_query():
 
 def project_query():
     return "project"
+
+def get_AST_graph(project_name):
+    return f"cpg.method.name(\"{project_name}\").dotAst.headOption.getOrElse(\"No AST found for this method\")"
+
+
+def get_CFG_graph(project_name):
+    return f"cpg.method.name(\"{project_name}\").dotCfg.headOption.getOrElse(\"No CFG found for this method\")"
+
+def get_CDG_graph(project_name):
+    return f"cpg.method.name(\"{project_name}\").dotCdg.headOption.getOrElse(\"No CDG found for this method\")"
+
+def get_DDG_graph(project_name):
+    return f"cpg.method.name(\"{project_name}\").dotDdg.headOption.getOrElse(\"No DDG found for this method\")"
+
+def get_PDG_graph(project_name):
+    return f"cpg.method.name(\"{project_name}\").dotPdg.headOption.getOrElse(\"No PDG found for this method\")"
+
+def get_CPG14_graph(project_name):
+    return f"cpg.method.name(\"{project_name}\").dotCpg14.headOption.getOrElse(\"No CPG14 found for this method\")"
+
+def get_ALL_graph(project_name):
+    return f"cpg.method.name(\"{project_name}\").dotAll.headOption.getOrElse(\"No ALL found for this method\")"
+
+def import_string_function_query(function: str) -> str:
+    return f'importCode.c.fromString("""{function}""")'
+
+def help_cpg():
+    return f"cpg.help"
